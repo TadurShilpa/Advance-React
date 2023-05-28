a7a6daef94a829aa478117a4d58ee10b5307e335
TadurShilpa <shilpa.tadur>
2023-05-28 15:46:51 +0530

    Edit Button useState

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -3,25 +3,37 @@ import Card from "../UI/Card";
 
 import ExpenseDate from "./ExpenseDate";
 import ExpenseDetails from "./ExpenseDetails";
+import { useState } from "react";
 
 function ExpenseItem(props) {
   const clickHandler = (e) => {
     console.log("clicked!!!!!!!!!!!!");
-    // const expenseItem = document.getElementById(`expense-${props.item.id}`);
-    // expenseItem.remove();
+
     let item = e.target.parentElement;
     item.remove();
+    item.remove();
   };
+  const [title, setTitle] = useState(props.item.title);
+  const [amount, setAmount] = useState(props.amount);
+
+  const editButton = (e) => {
+    setTitle("Updated!");
+    console.log(title);
+    setAmount("100$");
+    console.log(amount);
+  };
+
   return (
     <Card className="expense-item">
       <div className="expense-item">
         <ExpenseDate date={props.item.date} />
         <ExpenseDetails
-          title={props.item.title}
-          amount={props.item.amount}
-          location={props.item.locationOfExpenditure}
+          id={props.item.id}
+          title={title}
+          amount={amount}
         ></ExpenseDetails>
         <button onClick={clickHandler}>Delete Expense</button>
+        <button onClick={editButton}> Edit Expense</button>
       </div>
     </Card>
   );
