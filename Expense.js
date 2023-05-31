526a26c9317bc5cefe3eab2622bee6eabfa1694f
TadurShilpa <shilpa.tadur>
2023-05-31 10:17:08 +0530

    Task9 AdV Js

--- Expense.js
+++ Expense.js
@@ -1,36 +1,94 @@
+import React, { useState } from "react";
+
 import ExpenseItem from "./ExpenseItem";
-import Card from "./Card";
+import Card from "../UI/Card";
+import ExpenseFilter from "./ExpenseFilter";
 import "./Expense.css";
 
-function Expenses(props) {
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <>
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-        LocationOfExpenditure={props.items[0].locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-        LocationOfExpenditure={props.items[1].locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-        LocationOfExpenditure={props.items[2].locationOfExpenditure}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-        LocationOfExpenditure={props.items[3].locationOfExpenditure}
-      />
-    </>
+    <div className="expenses">
+      <Card className="expenses">
+        <ExpenseFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpenseItem
+          title={props.items[0].title}
+          amount={props.items[0].amount}
+          date={props.items[0].date}
+        />
+        <ExpenseItem
+          title={props.items[1].title}
+          amount={props.items[1].amount}
+          date={props.items[1].date}
+        />
+        <ExpenseItem
+          title={props.items[2].title}
+          amount={props.items[2].amount}
+          date={props.items[2].date}
+        />
+        <ExpenseItem
+          title={props.items[3].title}
+          amount={props.items[3].amount}
+          date={props.items[3].date}
+        />
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expenses;
+
+// import React from "react";
+// import ExpenseItem from "./ExpenseItem";
+// // import Card from "../UI/Card";
+// import "./Expense.css";
+
+// const Expenses = (props) => {
+//   return (
+//     // <div className="expenses">
+//     //   <ExpenseItem
+//     //   title={props.items[0].title}
+//     //   amount={props.items[0].amount}
+//     //   date={props.items[0].date}
+//     //   //LocationOfExpenditure={props.items[0].locationOfExpenditure}
+//     // />
+//     // <ExpenseItem
+//     //   title={props.items[1].title}
+//     //   amount={props.items[1].amount}
+//     //   date={props.items[1].date}
+//     //   //LocationOfExpenditure={props.items[1].locationOfExpenditure}
+//     // />
+//     // <ExpenseItem
+//     //   title={props.items[2].title}
+//     //   amount={props.items[2].amount}
+//     //   date={props.items[2].date}
+//     //   //LocationOfExpenditure={props.items[2].locationOfExpenditure}
+//     // />
+//     // <ExpenseItem
+//     //   title={props.items[3].title}
+//     //   amount={props.items[3].amount}
+//     //   date={props.items[3].date}
+//     //   //LocationOfExpenditure={props.items[3].locationOfExpenditure}
+//     // />
+//     <div className="expenses">
+//       {props.items.map((expense) => (
+//         <ExpenseItem
+//           key={expense.id}
+//           title={expense.title}
+//           amount={expense.amount}
+//           date={expense.date}
+//         />
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Expenses;
--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,41 +1,108 @@
-import "./ExpenseItem.css";
-import Card from "../UI/Card";
+//Task 9
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
-import ExpenseDetails from "./ExpenseDetails";
-import { useState } from "react";
+//import Card from "../UI/Card";
+import "./ExpenseItem.css";
 
-function ExpenseItem(props) {
-  const clickHandler = (e) => {
-    console.log("clicked!!!!!!!!!!!!");
+const ExpenseItem = (props) => {
+  // function clickHandler() {}
+  const [title, setTitle] = useState(props.title);
+  console.log("ExpenseItem evaluated by React");
 
-    let item = e.target.parentElement;
-    item.remove();
-    item.remove();
-  };
-  const [title, setTitle] = useState(props.item.title);
-  const [amount, setAmount] = useState(props.amount);
-
-  const editButton = (e) => {
+  const clickHandler = () => {
     setTitle("Updated!");
     console.log(title);
-    setAmount("100$");
-    console.log(amount);
   };
 
   return (
-    <Card className="expense-item">
-      <div className="expense-item">
-        <ExpenseDate date={props.item.date} />
-        <ExpenseDetails
-          id={props.item.id}
-          title={title}
-          amount={amount}
-        ></ExpenseDetails>
-        <button onClick={clickHandler}>Delete Expense</button>
-        <button onClick={editButton}> Edit Expense</button>
+    //Card
+    <div className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
-    </Card>
+      <button onClick={clickHandler}>Change Title</button>
+    </div>
   );
-}
+};
+
 export default ExpenseItem;
+
+//task8
+// import React, { useState } from "react";
+
+// import ExpenseDate from "./ExpenseDate";
+// //import Card from "../UI/Card";
+// import "./ExpenseItem.css";
+
+// const ExpenseItem = (props) => {
+//   // function clickHandler() {}
+//   const [title, setTitle] = useState(props.title);
+//   console.log("ExpenseItem evaluated by React");
+
+//   const clickHandler = () => {
+//     setTitle("Updated!");
+//     console.log(title);
+//   };
+
+//   return (
+//     <div className="expense-item">
+//       <ExpenseDate date={props.date} />
+//       <div className="expense-item__description">
+//         <h2>{title}</h2>
+//         <div className="expense-item__price">${props.amount}</div>
+//       </div>
+//       <button onClick={clickHandler}>Change Title</button>
+//     </div>
+//   );
+// };
+
+// export default ExpenseItem;
+
+// import "./ExpenseItem.css";
+// import Card from "../UI/Card";
+
+// import ExpenseDate from "./ExpenseDate";
+// import ExpenseDetails from "./ExpenseDetails";
+// import { useState } from "react";
+
+// function ExpenseItem(props) {
+//   const clickHandler = (e) => {
+//     console.log("clicked!!!!!!!!!!!!");
+
+//     let item = e.target.parentElement;
+//     item.remove();
+//     item.remove();
+//   };
+//   const [title, setTitle] = useState(props.item.title);
+//   const [amount, setAmount] = useState(props.amount);
+
+//   const editButton = (e) => {
+//     setTitle("Updated!");
+//     console.log(title);
+//     setAmount("100$");
+//     //console.log(amount);
+//   };
+
+//   return (
+//     <Card className="expense-item">
+//       <div className="expense-item">
+//         <ExpenseDate date={props.item.date} />
+//         <ExpenseDetails
+//           // id={props.item.id}
+//           // title={props.item.title}
+//           // amount={props.item.amount}
+//           //  location={props.location}
+//           id={props.item.id}
+//           title={title} // Use the updated title state
+//           amount={amount} // Use the updated amount state
+//         ></ExpenseDetails>
+//         <button onClick={clickHandler}>Delete Expense</button>
+//         <button onClick={editButton}> Edit Expense</button>
+//       </div>
+//     </Card>
+//   );
+// }
+// export default ExpenseItem;
