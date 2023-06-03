5b1d09cd4881dde4e1c08e281fc263d324516cac
TadurShilpa <shilpa.tadur>
2023-06-03 08:30:52 +0530

    Task10 Adv JS

--- Expense.js
+++ Expense.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
-import Card from "../UI/Card";
+// import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import "./Expense.css";
 
@@ -14,32 +14,18 @@ const Expenses = (props) => {
 
   return (
     <div className="expenses">
-      <Card className="expenses">
-        <ExpenseFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
+      <ExpenseFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {props.items.map((expense) => (
         <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
         />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
-      </Card>
+      ))}
     </div>
   );
 };
