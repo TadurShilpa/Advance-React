e3f8742e186854a4402260d17344bff419516232
TadurShilpa <shilpa.tadur>
2023-06-12 18:18:36 +0530

    Task13 AdvJS

--- ExpenseForm.js
+++ ExpenseForm.js
@@ -67,6 +67,9 @@ const ExpenseForm = (props) => {
         </div>
 
         <div className="new-expense__actions">
+          <button type="button" onClick={props.onCancel}>
+            Cancel
+          </button>
           <button type="Submit">Add Expense</button>
         </div>
       </div>
